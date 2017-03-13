30f63230497e97953a5800daf758c1073cea7b1c
Harshul Mulchandani <harshul@Harshuls-MBP.socal.rr.com>
2017-03-13 13:49:01 -0700

    basic communication

---
+++ ReactNativeIntegrationDemo/RNStuffManager.m
@@ -0,0 +1,9 @@
+//
+//  RNStuffManager.m
+//  ReactNativeIntegrationDemo
+//
+//  Created by Harshul Mulchandani on 3/13/17.
+//  Copyright © 2017 Harshul Mulchandani. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
---
+++ ReactNativeIntegrationDemo/RNStuffManager.swift
@@ -0,0 +1,9 @@
+//
+//  RNStuffManager.swift
+//  ReactNativeIntegrationDemo
+//
+//  Created by Harshul Mulchandani on 3/13/17.
+//  Copyright © 2017 Harshul Mulchandani. All rights reserved.
+//
+
+import Foundation
